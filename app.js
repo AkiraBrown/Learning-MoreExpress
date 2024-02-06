425cf689ea31f6f139bae3930bea3eab0de5f804
AkiraBrown <akira.watson.brown@gmail.com>
2024-02-06 12:52:41 -0500

    Added header configuration using cors

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const userController = require("./controller/userController");
+const cors = require("cors");
 const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("Hello");
